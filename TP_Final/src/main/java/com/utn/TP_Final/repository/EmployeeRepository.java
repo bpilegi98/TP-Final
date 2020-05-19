6870dacba7f610f57fe00a12ef36517303cf3f2c
Bianca Pilegi <46908195+bpilegi98@users.noreply.github.com>
2020-05-18 23:27:21 -0300

    Delete EmployeeRepository.java

--- TP_Final/src/main/java/com/utn/TP_Final/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.utn.TP_Final.repository;
-
-import com.utn.TP_Final.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
